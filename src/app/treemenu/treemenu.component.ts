6e9b9b98c90121686f8a36dac42ed6fe622d05ec
mm_dev <bowwck@gmail.com>
2019-09-02 18:44:33 +0300

    push

--- src/app/treemenu/treemenu.component.ts
+++ src/app/treemenu/treemenu.component.ts
@@ -63,16 +63,7 @@ export class TreemenuComponent implements OnInit {
       }
       return arrtree;
     }else{
-      treedata[0] = {name: "", title: "", color: "", bordercolor: "", children: [], rpanel: []};
-      treedata[0].name = data.name;
-      treedata[0].title = data.type;
-      treedata[0].rpanel = data.info;
-      treedata[0].color = arrcolor[0];
-      treedata[0].bordercolor = arrcolor[0];
-      if (data.children != undefined){
-        treedata[0].children = this.convertJson2Tree(data.children, depth+1);
-      }
-      return treedata;
+      
     }
   }
 
